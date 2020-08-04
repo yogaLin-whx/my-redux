a27e81a865794841346ea752c493d92273fb7c8a
WUCH8 <944204910@qq.com>
2020-08-04 14:37:32 +0800

    [jayden chris]feature:initialize project

---
+++ src/component/Item/Item.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Item extends React.Component {
+
+    render() {
+
+
+
+        return (<div>
+            <div>
+                <input /><button>Add</button>
+            </div>
+        </div>
+        )
+    }
+
+}
+
+export default Item;
